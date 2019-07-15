29666ef359ab72170602c4b0b6bf0a546cc7a189
Behrouz Derakhshan <behrouz.derakhshan@dfki.de>
2019-07-15 13:54:58 +0200

    adds materialization rates to the run same workload experiment

--- code/collaborative-optimizer/paper/experiment_workloads/reuse/run-on-node.sh
+++ code/collaborative-optimizer/paper/experiment_workloads/reuse/run-on-node.sh
@@ -21,19 +21,22 @@ mv application_test.csv original_train_test/
 python collaborative-optimization/code/collaborative-optimizer/paper/experiment_workloads/reuse/kaggle_home_credit/competition_preprocessing.py '/home/behrouz/collaborative-optimization/code/collaborative-optimizer/' 'root=/home/behrouz/collaborative-optimization'
 
 
+#### Same Workload All Experiments ####
 # Runing on Local machine
 python runner-same-workload.py '/Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/' 'root=/Users/bede01/Documents/work/phd-papers/ml-workload-optimization/' \
-'experiment=kaggle_home_credit' 'workload=start_here_a_gentle_introduction' 'mode=local' 'rep=3'
+'experiment=kaggle_home_credit' 'workload=start_here_a_gentle_introduction' 'mode=local' 'rep=2' 'mat_rate=0.75' 'run_baseline=yes'
 
-# Running on cloud
+### Running on cloud ###
 python collaborative-optimization/code/collaborative-optimizer/paper/experiment_workloads/reuse/runner-same-workload.py '/home/behrouz/collaborative-optimization/code/collaborative-optimizer/' 'root=/home/behrouz/collaborative-optimization' \
-'experiment=kaggle_home_credit' 'workload=introduction_to_manual_feature_engineering' 'mode=remote' 'rep=3'
-
-python collaborative-optimization/code/collaborative-optimizer/paper/experiment_workloads/reuse/runner-same-workload.py '/home/behrouz/collaborative-optimization/code/collaborative-optimizer/' 'root=/home/behrouz/collaborative-optimization' \
-'experiment=kaggle_home_credit' 'workload=introduction_to_manual_feature_engineering_p2' 'mode=remote' 'rep=3'
+'experiment=kaggle_home_credit' 'workload=introduction_to_manual_feature_engineering' 'mode=remote' 'rep=2' 'mat_rate=[1.0, 0.75, 0.50, 0.25, 0.0]' 'run_baseline=yes'
 
 python collaborative-optimization/code/collaborative-optimizer/paper/experiment_workloads/reuse/runner-same-workload.py '/home/behrouz/collaborative-optimization/code/collaborative-optimizer/' 'root=/home/behrouz/collaborative-optimization' \
-'experiment=kaggle_home_credit' 'workload=start_here_a_gentle_introduction' 'mode=remote' 'rep=3'
+'experiment=kaggle_home_credit' 'workload=introduction_to_manual_feature_engineering_p2' 'mode=remote' 'rep=2' 'mat_rate=[1.0, 0.75, 0.50, 0.25, 0.0]' 'run_baseline=yes'
+
+
+python collaborative-optimization/code/collaborative-optimizer/paper/experiment_workloads/reuse/runner-same-workload.py '/home/behrouz/collaborative-optimization/code/collaborative-optimizer/' 'root=/home/behrouz/collaborative-optimization' \
+'experiment=kaggle_home_credit' 'workload=start_here_a_gentle_introduction' 'mode=remote' 'rep=2' 'mat_rate=[1.0, 0.75, 0.50, 0.25, 0.0]' 'run_baseline=yes'
+
 
 
 scp -r behrouz@cloud-41.dima.tu-berlin.de:/home/behrouz/collaborative-optimization/experiment_results/ /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/experiment_results
--- code/collaborative-optimizer/paper/experiment_workloads/reuse/runner-same-workload.py
+++ code/collaborative-optimizer/paper/experiment_workloads/reuse/runner-same-workload.py
@@ -25,10 +25,12 @@ from experiment_graph.benchmark_helper import BenchmarkMetrics
 from paper.experiment_helper import Parser
 
 parser = Parser(sys.argv)
-verbose = parser.get('verbose', 0)
+verbose = int(parser.get('verbose', 0))
 ROOT = parser.get('root', '/Users/bede01/Documents/work/phd-papers/ml-workload-optimization/')
 # Experiment Graph
+
 from experiment_graph.execution_environment import ExecutionEnvironment
+from experiment_graph.materialization_algorithms.materialization_methods import StorageAwareMaterializer
 
 EXPERIMENT = parser.get('experiment', 'kaggle_home_credit')
 WORKLOAD = parser.get('workload', 'introduction_to_manual_feature_engineering_p2')
@@ -40,7 +42,8 @@ RESULT_FOLDER = ROOT + '/experiment_results/{}/reuse/same-workload/{}'.format(MO
 OUTPUT_CSV = RESULT_FOLDER + '/experiment_results.csv'
 REP = int(parser.get('rep', 2))
 # what percentage of the total artifact size to materialize
-MAT_RATE = float(parser.get('mat', 1.0))
+MATERIALIZATION_RATES = [float(m) for m in parser.get('mat_rates', '1.0').split(',')]
+RUN_BASELINE = parser.get('run_baseline', 'no')
 
 # unique identifier for the experiment run
 e_id = uuid.uuid4().hex.upper()[0:8]
@@ -57,38 +60,56 @@ if os.path.isdir(DATABASE_PATH):
     ee.load_history_from_disk(DATABASE_PATH)
 else:
     print 'No Experiment Graph Exists!!!'
+for mat_rate in MATERIALIZATION_RATES:
+    for i in range(1, REP + 1):
+        print 'Run Number {}, for script {}, with materialization rate: {}'.format(i, WORKLOAD, mat_rate)
+        # Running Optimized Workload 1 and storing the run time
+        execution_start = datetime.now()
+        print '{}-Start of the Optimized Workload'.format(execution_start)
+        optimized_workload = import_module(EXPERIMENT + '.optimized.' + WORKLOAD)
+        ee.new_workload()
+        optimized_workload.run(ee, ROOT_DATA_DIRECTORY, verbose=verbose)
+        ee.update_history()
+        execution_end = datetime.now()
+        elapsed = (execution_end - execution_start).total_seconds()
+        print '{}-End of Optimized Workload'.format(execution_end)
+
+        with open(OUTPUT_CSV, 'a') as the_file:
+            # get_benchmark_results has the following order:
+            the_file.write('{},{},{},{},{}, optimized,{}\n'.format(e_id, i, EXPERIMENT, WORKLOAD, mat_rate, elapsed))
+
+        with open(RESULT_FOLDER + '/details/{}.csv'.format(e_id), 'a') as result:
+            result.write(ee.get_benchmark_results() + "\n")
+        # Code for materialization
+        total_graph_size = ee.history_graph.get_total_size()
+
+        if mat_rate != 1.0:
+            budget = mat_rate * total_graph_size
+            print 'total size {}, materialization rate: {}, budget: {}'.format(total_graph_size, mat_rate, budget)
+            sa_materializer = StorageAwareMaterializer(execution_environment=ee, storage_budget=budget, verbose=False)
+            sa_materializer.run_and_materialize()
+            print 'after materialization graph size: {}, data storage size: {}, real size: {}'.format(
+                ee.history_graph.get_total_materialized_size(), ee.data_storage.total_size(),
+                ee.get_real_history_graph_size())
+        elif mat_rate == 0.0:
+            print 'materialization rate is 0.0'
+            del ee
+            ee = ExecutionEnvironment()
+        else:
+            print 'ignoring materialization, since materialization rate is 1.0'
 
 for i in range(1, REP + 1):
-    print 'Run Number {}'.format(i)
-    # Running Optimized Workload 1 and storing the run time
-    execution_start = datetime.now()
-    print '{}-Start of the Optimized Workload'.format(execution_start)
-    optimized_workload = import_module(EXPERIMENT + '.optimized.' + WORKLOAD)
-    ee.new_workload()
-    optimized_workload.run(ee, ROOT_DATA_DIRECTORY, verbose=verbose)
-    ee.update_history()
-    execution_end = datetime.now()
-    elapsed = (execution_end - execution_start).total_seconds()
-    print '{}-End of Optimized Workload'.format(execution_end)
-
-    with open(OUTPUT_CSV, 'a') as the_file:
-        # get_benchmark_results has the following order:
-        the_file.write('{},{},{},{},optimized,{}\n'.format(e_id, i, EXPERIMENT, WORKLOAD, elapsed))
-
-    with open(RESULT_FOLDER + '/details/{}.csv'.format(e_id), 'a') as result:
-        result.write(ee.get_benchmark_results() + "\n")
-
-    # # Running Baseline Workload 1 and storing the run time
+    # Running Baseline Workload and storing the run time
     execution_start = datetime.now()
     print '{}-Start of the Baseline Workload'.format(execution_start)
     baseline_workload = import_module(EXPERIMENT + '.baseline.' + WORKLOAD)
     baseline_workload.run(ROOT_DATA_DIRECTORY)
     execution_end = datetime.now()
-    elapsed = (execution_end - execution_start).total_seconds()
+    elapsed = (execution_start - execution_start).total_seconds()
     print '{}-End of Baseline Workload in {} seconds'.format(execution_end, elapsed)
 
     with open(OUTPUT_CSV, 'a') as the_file:
         the_file.write(
             '{},{},{},{},baseline,{}\n'.format(e_id, i, EXPERIMENT, WORKLOAD, elapsed))
+
     # End of Baseline Workload 1
-# ee.save_history(DATABASE_PATH)
