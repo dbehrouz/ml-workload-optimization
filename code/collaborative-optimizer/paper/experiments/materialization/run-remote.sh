8649dfe7af3873bf75715a3ddc182f71e22335dd
d-behi <behrouz.derakhshan@gmail.com>
2019-10-04 12:14:32 +0200

    adds figures of materializaiotn experiment

--- code/collaborative-optimizer/paper/experiments/materialization/run-remote.sh
+++ code/collaborative-optimizer/paper/experiments/materialization/run-remote.sh
@@ -7,44 +7,43 @@ experiment='kaggle_home_credit'
 root='/home/behrouz/collaborative-optimization'
 result_path=${root}'/experiment_results/remote/materialization/kaggle_home_credit/'${file_name}'/'${current_date}'.csv'
 
-python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=0.0' 'method=optimized' 'materializer=storage_aware'
-
-python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=0.0' 'method=optimized' 'materializer=simple'
-
+#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=0.0' 'method=optimized' 'materializer=storage_aware'
+#
+#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=0.0' 'method=optimized' 'materializer=simple'
+#
 python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
 ${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
 'mat_budget=16.0' 'method=optimized' 'materializer=storage_aware'
-
-python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=16.0' 'method=optimized' 'materializer=simple'
-
-python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=32.0' 'method=optimized' 'materializer=storage_aware'
-
-python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=32.0' 'method=optimized' 'materializer=simple'
-
-
-python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=64.0' 'method=optimized' 'materializer=storage_aware'
-
-python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=64.0' 'method=optimized' 'materializer=simple'
-
-
-python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=128.0' 'method=optimized' 'materializer=storage_aware'
-
-python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=128.0' 'method=optimized' 'materializer=simple'
+#
+#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=16.0' 'method=optimized' 'materializer=simple'
+#
+#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=32.0' 'method=optimized' 'materializer=storage_aware'
+#
+#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=32.0' 'method=optimized' 'materializer=simple'
+#
+#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=64.0' 'method=optimized' 'materializer=storage_aware'
+#
+#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=64.0' 'method=optimized' 'materializer=simple'
+#
+#
+#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=128.0' 'method=optimized' 'materializer=storage_aware'
+#
+#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=128.0' 'method=optimized' 'materializer=simple'
