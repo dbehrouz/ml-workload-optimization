98818c39ff2809f085e7acf2d808cb42c185e2c6
d-behi <behrouz.derakhshan@gmail.com>
2019-10-07 10:16:33 +0200

    modifies local run script

--- code/collaborative-optimizer/paper/experiments/materialization/run-local.sh
+++ code/collaborative-optimizer/paper/experiments/materialization/run-local.sh
@@ -7,32 +7,32 @@ result_path=${root}'/experiment_results/local/materialization/kaggle_home_credit
 
 python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
 ${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=1.0' 'method=mock_optimized' 'materializer=simple'
+'mat_budget=5.0' 'method=optimized' 'materializer=simple'
 
 python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
 ${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=1.0' 'method=mock_optimized' 'materializer=storage_aware'
-
-python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=5.0' 'method=mock_optimized' 'materializer=simple'
-
-python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=5.0' 'method=mock_optimized' 'materializer=storage_aware'
-
-python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=10.0' 'method=mock_optimized' 'materializer=simple'
-
-python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=10.0' 'method=mock_optimized' 'materializer=storage_aware'
-
-python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=20.0' 'method=mock_optimized' 'materializer=simple'
-
-python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-'mat_budget=20.0' 'method=mock_optimized' 'materializer=storage_aware'
+'mat_budget=5.0' 'method=optimized' 'materializer=storage_aware'
+#
+#python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=5.0' 'method=mock_optimized' 'materializer=simple'
+#
+#python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=5.0' 'method=mock_optimized' 'materializer=storage_aware'
+#
+#python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=10.0' 'method=mock_optimized' 'materializer=simple'
+#
+#python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=10.0' 'method=mock_optimized' 'materializer=storage_aware'
+#
+#python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=20.0' 'method=mock_optimized' 'materializer=simple'
+#
+#python /Users/bede01/Documents/work/phd-papers/ml-workload-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+#'mat_budget=20.0' 'method=mock_optimized' 'materializer=storage_aware'
