dc574ae1f60d29bd46432a763f1c852f0cad7efd
d-behi <behrouz.derakhshan@gmail.com>
2019-10-15 09:11:50 +0200

    finalzies warmstarting text and add ALL mat for the materialization run time experiment

--- code/collaborative-optimizer/paper/experiments/materialization/run-remote.sh
+++ code/collaborative-optimizer/paper/experiments/materialization/run-remote.sh
@@ -7,14 +7,6 @@ experiment='kaggle_home_credit'
 root='/home/behrouz/collaborative-optimization'
 result_path=${root}'/experiment_results/remote/materialization/kaggle_home_credit/'${file_name}'/'${current_date}'.csv'
 
-#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-#'mat_budget=0.0' 'method=optimized' 'materializer=storage_aware' 'profile='${root}'/data/profiles/cloud-41-dedup'
-#
-#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-#'mat_budget=0.0' 'method=optimized' 'materializer=simple' 'profile='${root}'/data/profiles/cloud-41-dedup'
-
 python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
 ${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
 'mat_budget=8.0' 'method=optimized' 'materializer=storage_aware' 'profile='${root}'/data/profiles/cloud-41-dedup'
@@ -47,11 +39,7 @@ python ~/collaborative-optimization/code/collaborative-optimizer/paper/experimen
 ${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
 'mat_budget=64.0' 'method=optimized' 'materializer=simple' 'profile='${root}'/data/profiles/cloud-41-dedup'
 
-#
-#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-#'mat_budget=128.0' 'method=optimized' 'materializer=storage_aware'
-#
-#python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
-#${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
-#'mat_budget=128.0' 'method=optimized' 'materializer=simple'
+python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/materialization/run_experiment.py \
+${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} \
+ 'method=optimized' 'materializer=all' 'profile='${root}'/data/profiles/cloud-41-dedup'
+
