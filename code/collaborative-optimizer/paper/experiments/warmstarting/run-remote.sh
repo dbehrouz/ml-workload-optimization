dd0fe3df973a86653e61931f4d450f2857a0dc97
d-behi <behrouz.derakhshan@gmail.com>
2019-10-21 18:03:45 +0200

    addresses Ziawasch's comments, and states the difference between EG and connected component

--- code/collaborative-optimizer/paper/experiments/warmstarting/run-remote.sh
+++ code/collaborative-optimizer/paper/experiments/warmstarting/run-remote.sh
@@ -8,7 +8,7 @@ root='/home/behrouz/collaborative-optimization'
 result_path=${root}'/experiment_results/remote/warmstarting/openml/'${file_name}'/'${current_date}'.csv'
 
 python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/warmstarting/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} 'mat_budget=2.0' \
+${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} 'mat_budget=0.1' \
 'method=optimized' 'limit=2000' 'profile='${root}'/data/profiles/cloud-41-dedup'
 
 python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/warmstarting/run_experiment.py \
@@ -16,5 +16,29 @@ ${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} '
 'limit=2000' 'profile='${root}'/data/profiles/cloud-41-dedup'
 
 python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/warmstarting/run_experiment.py \
-${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} 'mat_budget=2.0' \
+${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} 'mat_budget=0.1' \
+'method=optimized' 'limit=2000' 'profile='${root}'/data/profiles/cloud-41-dedup' 'warmstart=0'
+
+python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/warmstarting/run_experiment.py \
+${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} 'mat_budget=0.1' \
+'method=optimized' 'limit=2000' 'profile='${root}'/data/profiles/cloud-41-dedup'
+
+python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/warmstarting/run_experiment.py \
+${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} 'method=baseline' \
+'limit=2000' 'profile='${root}'/data/profiles/cloud-41-dedup'
+
+python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/warmstarting/run_experiment.py \
+${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} 'mat_budget=0.1' \
+'method=optimized' 'limit=2000' 'profile='${root}'/data/profiles/cloud-41-dedup' 'warmstart=0'
+
+python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/warmstarting/run_experiment.py \
+${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} 'mat_budget=0.1' \
+'method=optimized' 'limit=2000' 'profile='${root}'/data/profiles/cloud-41-dedup'
+
+python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/warmstarting/run_experiment.py \
+${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} 'method=baseline' \
+'limit=2000' 'profile='${root}'/data/profiles/cloud-41-dedup'
+
+python ~/collaborative-optimization/code/collaborative-optimizer/paper/experiments/warmstarting/run_experiment.py \
+${root}'/code/collaborative-optimizer/' 'root='${root} 'result='${result_path} 'experiment='${experiment} 'mat_budget=0.1' \
 'method=optimized' 'limit=2000' 'profile='${root}'/data/profiles/cloud-41-dedup' 'warmstart=0'
