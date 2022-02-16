a8f58f7cc48359a94c42041148c5ab379191f192
d-behi <behrouz.derakhshan@gmail.com>
2022-02-16 14:29:54 +0100

    adds unit test for simple userdefined function

---
+++ code/collaborative-optimizer/tests/test_operations.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+
+import pandas as pd
+
+from execution_environment import ExecutionEnvironment, UserDefinedFunction
+
+
+class TestExecutionEnvironment(TestCase):
+    def setUp(self) -> None:
+        self.execution_environment = ExecutionEnvironment()
+
+    def test_simple_user_defined_operators(self):
+        class ClipDataset(UserDefinedFunction):
+            def __init__(self, lower=None, upper=None, axis=None):
+                super().__init__(return_type='Dataset')
+                self.lower = lower
+                self.upper = upper
+                self.axis = axis
+
+            def run(self, underlying_data):
+                # here the underlying_data is a pandas dataframe and we are directly calling the pandas clip function
+                return underlying_data.clip(lower=self.lower, upper=self.upper, axis=self.axis)
+
+        data = self.execution_environment.load('data/sample.csv')
+
+        clip_oper = ClipDataset(lower=2, upper=4)
+        clipped = data.run_udf(clip_oper)
+
+        expected = pd.DataFrame.from_dict({'a': [2, 4, 2], 'b': [2, 4, 2], 'c': [3, 4, 2], 'd': [4, 4, 3]})
+        pd.testing.assert_frame_equal(clipped.data(), expected)
+
+    
