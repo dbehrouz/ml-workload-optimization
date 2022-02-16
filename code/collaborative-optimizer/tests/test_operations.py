6209c1306a74e01a9f7d6120d1a70f4d87cb155d
d-behi <behrouz.derakhshan@gmail.com>
2022-02-16 14:35:08 +0100

    test if multiinput udf throws exception if others not set

--- code/collaborative-optimizer/tests/test_operations.py
+++ code/collaborative-optimizer/tests/test_operations.py
@@ -2,14 +2,14 @@ from unittest import TestCase
 
 import pandas as pd
 
-from execution_environment import ExecutionEnvironment, UserDefinedFunction
+from execution_environment import ExecutionEnvironment, UserDefinedFunction, MultiInputUserDefinedFunction
 
 
 class TestExecutionEnvironment(TestCase):
     def setUp(self) -> None:
         self.execution_environment = ExecutionEnvironment()
 
-    def test_simple_user_defined_operators(self):
+    def test_simple_udf(self):
         class ClipDataset(UserDefinedFunction):
             def __init__(self, lower=None, upper=None, axis=None):
                 super().__init__(return_type='Dataset')
@@ -28,5 +28,20 @@ class TestExecutionEnvironment(TestCase):
 
         expected = pd.DataFrame.from_dict({'a': [2, 4, 2], 'b': [2, 4, 2], 'c': [3, 4, 2], 'd': [4, 4, 3]})
         pd.testing.assert_frame_equal(clipped.data(), expected)
+
+    def test_multi_input_udf(self):
+        class OthersNotSet(MultiInputUserDefinedFunction):
+            def __init__(self):
+                super().__init__(return_type='Dataset')
+
+            def run(self, underlying_data):
+                # here the underlying_data is a pandas dataframe and we are directly calling the pandas clip function
+                return underlying_data
+
+        with self.assertRaises(ValueError):
+            data = self.execution_environment.load('data/sample.csv')
+            multi_input_oper = OthersNotSet()
+            result = data.run_udf(multi_input_oper)
+            result.data()
 
     
